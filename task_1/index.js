f3a113f88bf01eeee119712c5fbfa44e6d04b385
= <=>
2026-09-06 11:30:36 +0300

    refactor

--- task_1/index.js
+++ task_1/index.js
@@ -6,10 +6,6 @@ const initArray = [
 
 const ids = [...new Set(initArray.map(obj => obj.id))];
 
-const uniqueArray = [];
-ids.forEach((id) => {
-    const findedElement = initArray.find(obj => id === obj.id);
-    uniqueArray.push(findedElement);
-});
+const result = ids.map(id => initArray.find(obj => id === obj.id));
 
-console.log(uniqueArray);
+console.log(result);
